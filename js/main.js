a2ba5b4dadb71e05b7c0a5bb0218358dc98c48eb
Diego Marins <diegomarins33@gmail.com>
2022-06-27 19:51:42 -0300

    fim do curso introdutório

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-var nome = "Diego Marins";
-var idade = 29;
-alert(nome + "tem " + idade + " anos");
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    console.log(document.getElementById("agradecimento"));
+}
+
+function redirecionar(){
+    window.open("https://www.google.com");
+    //window.location.href = "https://www.google.com"; 
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse!";
+    elemento.innerHTML = "Obrigado por passar o mouse!"
+    //alert("trocar texto")
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse!";
+    elemento.innerHTML = "Passe o mouse aqui"
+}
+
+function load(){
+    alert("Página carregada.")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
